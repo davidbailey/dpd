cd98512905ea0cff0f3db4360726a969d85b11d9
David Bailey <davidbailey@users.noreply.github.com>
2017-11-23 00:22:59 +0100

    Update __init__.py

--- dpd/__init__.py
+++ dpd/__init__.py
@@ -1,12 +1,14 @@
 from ._version import __version__
 
+from .gtfs import url2gtfs
+from .gtfs import get_rail_stops
+from .gtfs import plot_stops
+
+from .overpass import query2rels
+
 from .uscensus import get_uscensus_population_by_tract
 from .uscensus import get_uscensus_geometry
 from .uscensus import add_density_to_tracts
 from .uscensus import get_uscensus_density_by_tract
 
 from .wikipedia import get_wikipedia_table
-
-from .gtfs import url2gtfs
-from .gtfs import get_rail_stops
-from .gtfs import plot_stops
