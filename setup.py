b5b1f30781f08733ac1d815e0137b4e6f6beebe4
David Bailey <davidbailey@users.noreply.github.com>
2017-11-23 10:23:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='DPD',
-      version='0.7',
+      version='0.8',
       description='A Python Transportation Toolkit',
       author='David Bailey',
       author_email='david@davidabailey.com',
