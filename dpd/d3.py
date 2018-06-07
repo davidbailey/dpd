ff76e73de1e99129c657b6f8cce7d2ee190e4a3c
David Bailey <davidbailey@users.noreply.github.com>
2018-06-07 15:07:29 -0700

    Update d3.py

--- dpd/d3.py
+++ dpd/d3.py
@@ -58,82 +58,74 @@ def radar_chart(legend_options, d, title):
 
 def dendrogram(d):
     template = Template('''
-<svg id="svg" width="1400" height="1000">
-  <style>
+    <script src="https://d3js.org/d3.v4.min.js"></script>
+    <svg id="svg" width="1400" height="1000">
+      <style>
 
-  .node circle {
-    fill: #999;
-  }
+      .node circle {
+        fill: #999;
+      }
 
-  .node text {
-    font: 10px sans-serif;
-  }
+      .node text {
+        font: 10px sans-serif;
+      }
 
-  .node--internal circle {
-    fill: #555;
-  }
+      .node--internal circle {
+        fill: #555;
+      }
 
-  .node--internal text {
-    text-shadow: 0 1px 0 #fff, 0 -1px 0 #fff, 1px 0 0 #fff, -1px 0 0 #fff;
-  }
+      .node--internal text {
+        text-shadow: 0 1px 0 #fff, 0 -1px 0 #fff, 1px 0 0 #fff, -1px 0 0 #fff;
+      }
 
-  .link {
-    fill: none;
-    stroke: #555;
-    stroke-opacity: 0.4;
-    stroke-width: 1.5px;
-  }
+      .link {
+        fill: none;
+        stroke: #555;
+        stroke-opacity: 0.4;
+        stroke-width: 1.5px;
+      }
 
-  </style>
-</svg>
-<a id="link">download .svg</a>
-<script src="https://d3js.org/d3.v4.min.js"></script>
-<script>
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    g = svg.append("g").attr("transform", "translate(440,0)");
+      </style>
+    </svg>
+    <script>
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        g = svg.append("g").attr("transform", "translate(440,0)");
 
-var tree = d3.cluster()
-    .size([height, width - 800]);
+    var tree = d3.cluster()
+        .size([height, width - 800]);
 
-var root = d3.hierarchy($d);
+    var root = d3.hierarchy($d);
 
-tree(root);
+    tree(root);
 
-var link = g.selectAll(".link")
-    .data(root.descendants().slice(1))
-  .enter().append("path")
-    .attr("class", "link")
-    .attr("d", function(d) {
-      return "M" + d.y + "," + d.x
-          + "C" + (d.parent.y + 100) + "," + d.x
-          + " " + (d.parent.y + 100) + "," + d.parent.x
-          + " " + d.parent.y + "," + d.parent.x;
-    });
+    var link = g.selectAll(".link")
+        .data(root.descendants().slice(1))
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("d", function(d) {
+          return "M" + d.y + "," + d.x
+              + "C" + (d.parent.y + 100) + "," + d.x
+              + " " + (d.parent.y + 100) + "," + d.parent.x
+              + " " + d.parent.y + "," + d.parent.x;
+        });
 
-var node = g.selectAll(".node")
-    .data(root.descendants())
-  .enter().append("g")
-    .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+    var node = g.selectAll(".node")
+        .data(root.descendants())
+      .enter().append("g")
+        .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
-node.append("circle")
-    .attr("r", 5);
+    node.append("circle")
+        .attr("r", 5);
 
-node.append("text")
-    .attr("dy", 8)
-    .attr("x", function(d) { return d.children ? -8 : 8; })
-    .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-    .style("font-size", "18px")
-    .text(function(d) { return d.data.name});
-
-
-var svg = document.getElementById("svg");
-var serializer = new XMLSerializer();
-var source = serializer.serializeToString(svg);
-var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-document.getElementById("link").href = url;
-</script>
+    node.append("text")
+        .attr("dy", 8)
+        .attr("x", function(d) { return d.children ? -8 : 8; })
+        .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+        .style("font-size", "18px")
+        .text(function(d) { return d.data.name});
+    </script>
     ''')
     return template.substitute({'d': json.dumps(d)})
