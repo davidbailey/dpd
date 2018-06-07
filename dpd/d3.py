8d5c6809e017c3bab4332751f7b6e71b2cd619ec
David Bailey <davidbailey@users.noreply.github.com>
2018-06-07 15:35:42 -0700

    Update d3.py

--- dpd/d3.py
+++ dpd/d3.py
@@ -88,44 +88,53 @@ def dendrogram(d):
       </style>
     </svg>
     <script>
-    var svg = d3.select("svg"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height"),
-        g = svg.append("g").attr("transform", "translate(440,0)");
+    require.config({
+        paths: {
+            d3: "https://d3js.org/d3.v4.min"
+        }
+    });
 
-    var tree = d3.cluster()
-        .size([height, width - 800]);
+    require(["d3"], function(d3) {
 
-    var root = d3.hierarchy($d);
+        var svg = d3.select("svg"),
+            width = +svg.attr("width"),
+            height = +svg.attr("height"),
+            g = svg.append("g").attr("transform", "translate(440,0)");
 
-    tree(root);
+        var tree = d3.cluster()
+            .size([height, width - 800]);
 
-    var link = g.selectAll(".link")
-        .data(root.descendants().slice(1))
-      .enter().append("path")
-        .attr("class", "link")
-        .attr("d", function(d) {
-          return "M" + d.y + "," + d.x
-              + "C" + (d.parent.y + 100) + "," + d.x
-              + " " + (d.parent.y + 100) + "," + d.parent.x
-              + " " + d.parent.y + "," + d.parent.x;
-        });
+        var root = d3.hierarchy($d);
 
-    var node = g.selectAll(".node")
-        .data(root.descendants())
-      .enter().append("g")
-        .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+        tree(root);
 
-    node.append("circle")
-        .attr("r", 5);
+        var link = g.selectAll(".link")
+            .data(root.descendants().slice(1))
+          .enter().append("path")
+            .attr("class", "link")
+            .attr("d", function(d) {
+              return "M" + d.y + "," + d.x
+                  + "C" + (d.parent.y + 100) + "," + d.x
+                  + " " + (d.parent.y + 100) + "," + d.parent.x
+                  + " " + d.parent.y + "," + d.parent.x;
+            });
 
-    node.append("text")
-        .attr("dy", 8)
-        .attr("x", function(d) { return d.children ? -8 : 8; })
-        .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-        .style("font-size", "18px")
-        .text(function(d) { return d.data.name});
+        var node = g.selectAll(".node")
+            .data(root.descendants())
+          .enter().append("g")
+            .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+        node.append("circle")
+            .attr("r", 5);
+
+        node.append("text")
+            .attr("dy", 8)
+            .attr("x", function(d) { return d.children ? -8 : 8; })
+            .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+            .style("font-size", "18px")
+            .text(function(d) { return d.data.name});
+        }
     </script>
     ''')
     return template.substitute({'d': json.dumps(d)})
