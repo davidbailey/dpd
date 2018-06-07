a7c51aae1202e3b7ce9255861adc7050e8e7b1aa
David Bailey <davidbailey@users.noreply.github.com>
2018-06-07 14:32:46 -0700

    Update d3.py

--- dpd/d3.py
+++ dpd/d3.py
@@ -132,13 +132,6 @@ node.append("text")
 var svg = document.getElementById("svg");
 var serializer = new XMLSerializer();
 var source = serializer.serializeToString(svg);
-if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-}
-if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-    source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-}
-source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
 var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
 document.getElementById("link").href = url;
 </script>
