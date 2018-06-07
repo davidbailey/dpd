b9debd40b41a63fc8c91956f1efe1f90e57fdd3b
David Bailey <davidbailey@users.noreply.github.com>
2018-06-07 13:29:43 -0700

    Update d3.py

--- dpd/d3.py
+++ dpd/d3.py
@@ -107,16 +107,16 @@ var link = g.selectAll(".link")
     .attr("class", "link")
     .attr("d", function(d) {
       return "M" + d.y + "," + d.x
-	  + "C" + (d.parent.y + 100) + "," + d.x
-	  + " " + (d.parent.y + 100) + "," + d.parent.x
-	  + " " + d.parent.y + "," + d.parent.x;
+          + "C" + (d.parent.y + 100) + "," + d.x
+          + " " + (d.parent.y + 100) + "," + d.parent.x
+          + " " + d.parent.y + "," + d.parent.x;
     });
 
 var node = g.selectAll(".node")
     .data(root.descendants())
   .enter().append("g")
     .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
 node.append("circle")
     .attr("r", 5);
