2788d7cc2bf75498682d64f828c579f1ab6308c3
David Bailey <davidbailey@users.noreply.github.com>
2019-02-24 23:56:27 -0800

    Update test_wikipedia.py

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -4,7 +4,7 @@ from dpd.wikipedia import get_wikipedia_table
 
 class TestWikipedia(unittest.TestCase):
     def test_get_wikipedia_table(self):
-        states = get_wikipedia_table('https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code', 0)
+        states = get_wikipedia_table('https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code')
         states.index = states['Name']
         state = states['Numeric code']['California']
         self.assertEqual(state, '06')
