f4fd95c58135101f6ba7e54f10dc33bd6bddc176
David Bailey <david@davidabailey.com>
2019-12-12 11:22:30 -0800

    fix test to work with new pyproj

--- dpd/utils/_epsg4326_to_aea.py
+++ dpd/utils/_epsg4326_to_aea.py
@@ -1,9 +1,7 @@
 """
 epsg4326_to_aea: transform a geom from EPSG:4326 to Albers Equal Area
 """
-from functools import partial
-
-import pyproj
+from pyproj import CRS, Transformer
 from shapely.ops import transform
 
 
@@ -11,9 +9,7 @@ def epsg4326_to_aea(geom):
     """
     epsg4326_to_aea: transform a geom from EPSG:4326 to Albers Equal Area
     """
-    return transform(
-        partial(
-            pyproj.transform, pyproj.Proj(init="EPSG:4326"), pyproj.Proj(proj="aea")
-        ),
-        geom,
-    )
+    crs_4326 = CRS.from_epsg(4326)
+    aea = CRS.from_string("North America Albers Equal Area Conic")
+    transformer = Transformer.from_crs(crs_4326, aea, always_xy=True)
+    return transform(transformer.transform, geom)
--- tests/utils/test_epsg4326_to_aea.py
+++ tests/utils/test_epsg4326_to_aea.py
@@ -8,8 +8,8 @@ class TestEPSG4326_To_AEA(unittest.TestCase):
     def test_epsg4326_to_aea(self):
         point = Point(-118.481389, 34.021944)
         aea_point = epsg4326_to_aea(point)
-        self.assertEqual(aea_point.x, -8258079.413088454)
-        self.assertEqual(aea_point.y, 9514122.322348474)
+        self.assertEqual(aea_point.x, -1955052.147737653)
+        self.assertEqual(aea_point.y, -468997.4237058063)
 
 if __name__ == '__main__':
     unittest.main()
