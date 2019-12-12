96a7212964b863a2921b7f2a17aeba01fa0ec0c4
David Bailey <david@davidabailey.com>
2019-12-11 22:39:12 -0800

    fix test

--- tests/utils/test_epsg4326_to_aea.py
+++ tests/utils/test_epsg4326_to_aea.py
@@ -6,10 +6,10 @@ from dpd.utils import epsg4326_to_aea
 
 class TestEPSG4326_To_AEA(unittest.TestCase):
     def test_epsg4326_to_aea(self):
-        point = Point(1,1)
+        point = Point(-118.481389, 34.021944)
         aea_point = epsg4326_to_aea(point)
-        self.assertEqual(aea_point.x, 128294.42841956337)
-        self.assertEqual(aea_point.y, 96237.9213969674)
+        self.assertEqual(aea_point.x, -8258079.413088454)
+        self.assertEqual(aea_point.y, 9514122.322348474)
 
 if __name__ == '__main__':
     unittest.main()
