69ec46ea68482a5ca5b4fe759f2b286d0884a44b
David Bailey <david@davidabailey.com>
2020-03-04 10:37:37 -0800

    improve tests

--- tests/gtfs/test_gtfs.py
+++ tests/gtfs/test_gtfs.py
@@ -1,6 +1,6 @@
 import unittest
 
-from dpd.gtfs import url2gtfs, get_stops_by_route_type
+from dpd.gtfs import url2gtfs, get_stops_by_route_type, plot_schedule
 
 
 class TestGTFS(unittest.TestCase):
@@ -9,6 +9,7 @@ class TestGTFS(unittest.TestCase):
         self.assertEqual(self.feed.agency["agency_id"][0], "SRTD")
         self.type_zero_stops = get_stops_by_route_type(self.feed, 0)
         self.assertEqual(len(self.type_zero_stops), 102)
+        plot_schedule(self.feed, "533", "1")
 
 
 if __name__ == "__main__":
