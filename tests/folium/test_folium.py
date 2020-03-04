b9cc5cdc6d4197c421ff5ef40d3c470d0d6a191a
David Bailey <david@davidabailey.com>
2020-03-04 11:00:43 -0800

    formatting

--- tests/folium/test_folium.py
+++ tests/folium/test_folium.py
@@ -8,7 +8,11 @@ from dpd.folium import plot_stops
 
 class TestFolium(unittest.TestCase):
     def test_folium(self):
-        self.stops = {"stop_lat": 38.6258, "stop_lon": -121.475, "stop_name": "Test Stop"}
+        self.stops = {
+            "stop_lat": 38.6258,
+            "stop_lon": -121.475,
+            "stop_name": "Test Stop",
+        }
         self.stops = pandas.DataFrame(self.stops, index=[0])
         self.m = folium.Map()
         plot_stops(self.m, self.stops)
