bd147e3e1670b4f55b7445ee2388dfdc1044205f
David Bailey <david@davidabailey.com>
2020-03-04 10:26:27 -0800

    fix test

--- tests/gtfs/test_gtfs.py
+++ tests/gtfs/test_gtfs.py
@@ -2,10 +2,11 @@ import unittest
 
 from dpd.gtfs import url2gtfs, get_stops_by_route_type
 
+
 class TestGTFS(unittest.TestCase):
     def test_gtfs(self):
         self.feed = url2gtfs("http://iportal.sacrt.com/gtfs/SRTD/google_transit.zip")
-        self.assertEqual(feed.agency["agency_id"][0], 'SRTD')
+        self.assertEqual(self.feed.agency["agency_id"][0], "SRTD")
         self.type_zero_stops = get_stops_by_route_type(self.feed, 0)
         self.assertEqual(len(self.type_zero_stops), 102)
 
