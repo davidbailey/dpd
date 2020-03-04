1e6849f446b327ea97839c881f4ceebcddd78369
David Bailey <david@davidabailey.com>
2020-03-04 10:09:04 -0800

    general cleanup

---
+++ dpd/folium/plot_stops.py
@@ -0,0 +1,44 @@
+from functools import partial
+
+import folium
+import geopandas
+from matplotlib import pyplot as plt
+import mplleaflet
+from shapely.geometry import Point
+
+
+def _plot_linestring(foliumMap, row):
+    folium.PolyLine(
+        locations=zip(row["geometry"].xy[1], row["geometry"].xy[0]), color=row["color"]
+    ).add_to(foliumMap)
+
+
+def plot_stops(foliumMap, stops, markercolor="red", with_contour=False):
+    """
+    Plot a table of stops on a Folium map.
+    
+    Args:
+        foliumMap (folium.folium.Map): the map to plot the stops on
+        stops (pandas.DataFrame): the DataFrame that contains the stops to plot
+        color (str): marker color for the stops
+        with_contour (bool): if the stops should include a contour_plot representing walk times
+    """
+    if with_coutour:
+        fig, ax = plt.subplots()
+        stops.apply(
+            lambda row: contour_plot(
+                ax, Point(float(row["stop_lon"]), float(row["stop_lat"])), 0.025, 15
+            ),
+            axis=1,
+        )
+        geojson = mplleaflet.fig_to_geojson(fig=fig)
+        features = geopandas.GeoDataFrame.from_features(geojson["features"])
+        features.apply(partial(_plot_linestring, foliumMap), axis=1)
+    stops.apply(
+        lambda row: folium.Marker(
+            [row["stop_lat"], row["stop_lon"]],
+            popup=row["stop_name"],
+            icon=folium.Icon(color=markercolor),
+        ).add_to(foliumMap),
+        axis=1,
+    )
