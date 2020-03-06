85922651724b17da7e673a5eedde329acad11866
David Bailey <david@davidabailey.com>
2020-03-06 00:28:58 -0800

    formatting

--- dpd/driving/stop.py
+++ dpd/driving/stop.py
@@ -1,9 +1,10 @@
 from shapely.geometry import Point
 
+
 class Stop:
-    def __init__(self, location, name = ""):
+    def __init__(self, location, name=""):
         """
         Defines a public transportation stop
         """
-        self.location:Point = location
-        self.name:str = name
+        self.location: Point = location
+        self.name: str = name
