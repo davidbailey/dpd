bb0bb0953b0764f08f613062d6a495625ec7b6aa
David Bailey <david@davidabailey.com>
2020-03-06 06:58:50 -0800

    fix test

--- tests/gtfs/test_gtfs.py
+++ tests/gtfs/test_gtfs.py
@@ -8,7 +8,8 @@ from dpd.gtfs import get_stops_by_route_type, plot_schedule
 class TestGTFS(unittest.TestCase):
     def test_gtfs(self):
         self.feed = gtfs_kit.read_feed(
-            "http://iportal.sacrt.com/gtfs/SRTD/google_transit.zip"
+            "http://iportal.sacrt.com/gtfs/SRTD/google_transit.zip",
+            dist_units="mi"
         )
         self.assertEqual(self.feed.agency["agency_id"][0], "SRTD")
         self.type_zero_stops = get_stops_by_route_type(self.feed, 0)
