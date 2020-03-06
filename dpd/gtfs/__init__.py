69df9fdffec705a09a7d7a306a290afc9e091b14
David Bailey <david@davidabailey.com>
2020-03-06 00:38:41 -0800

    remove url2gtfs because it is now merged upstream

--- dpd/gtfs/__init__.py
+++ dpd/gtfs/__init__.py
@@ -1,3 +1,2 @@
 from .get_stops_by_route_type import get_stops_by_route_type
 from .plot_schedule import plot_schedule
-from .url2gtfs import url2gtfs
--- tests/gtfs/test_gtfs.py
+++ tests/gtfs/test_gtfs.py
@@ -1,11 +1,13 @@
 import unittest
 
-from dpd.gtfs import url2gtfs, get_stops_by_route_type, plot_schedule
+import gtfs_kit
+
+from dpd.gtfs import get_stops_by_route_type, plot_schedule
 
 
 class TestGTFS(unittest.TestCase):
     def test_gtfs(self):
-        self.feed = url2gtfs("http://iportal.sacrt.com/gtfs/SRTD/google_transit.zip")
+        self.feed = gtfs_kit.read_feed("http://iportal.sacrt.com/gtfs/SRTD/google_transit.zip")
         self.assertEqual(self.feed.agency["agency_id"][0], "SRTD")
         self.type_zero_stops = get_stops_by_route_type(self.feed, 0)
         self.assertEqual(len(self.type_zero_stops), 102)
