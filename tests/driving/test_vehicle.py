f90c005b9ba4419c15133ec63d7bcceb61c65320
David Bailey <david@davidabailey.com>
2020-03-13 13:23:07 -0700

    formatting

--- tests/driving/test_vehicle.py
+++ tests/driving/test_vehicle.py
@@ -2,6 +2,7 @@ import unittest
 
 from dpd.driving import Vehicle
 
+
 class TestVehicle(unittest.TestCase):
     def test_vehicle(self):
         self.vehicle = Vehicle(24.5872, 1.3, -1.3, name="max_speed_55_mph_vehicle")
