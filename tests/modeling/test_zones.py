f1d3dd4b3ae63e36505e43324731db0499f95a70
David Bailey <david@davidabailey.com>
2020-03-19 16:09:56 -0700

    adding tests

---
+++ tests/modeling/test_zones.py
@@ -0,0 +1,18 @@
+import unittest
+
+from dpd.modeling import Zones
+
+ST = "44"
+YEAR = "2017"
+BETA = 0.001
+
+
+class TestZones(unittest.TestCase):
+    def test_zones(self):
+        zones = Zones.from_uscensus(ST, YEAR)
+        zones["Attraction"] = zones["Production"]
+        od = zones.calculate_origin_destination_dataframe(beta=beta)
+
+
+if __name__ == "__main__":
+    unittest.main()
