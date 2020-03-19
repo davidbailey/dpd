7a463ac651d2d9122dba5ecf579f30089b45fb9a
David Bailey <david@davidabailey.com>
2020-03-19 16:10:14 -0700

    adding tests

---
+++ tests/modeling/test_origin_destination_dataframe.py
@@ -0,0 +1,15 @@
+import unittest
+
+from dpd.modeling import OriginDestinationDataFrame
+
+ST = "ri"
+YEAR = "2017"
+
+
+class TestOriginDestinationDataFrame(unittest.TestCase):
+    def test_origin_destination_dataframe(self):
+        od = OriginDestinationDataFrame.from_lodes(ST, YEAR)
+
+
+if __name__ == "__main__":
+    unittest.main()
