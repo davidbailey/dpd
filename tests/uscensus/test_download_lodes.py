0acb909ccb7677d2bdb4f4f60bafb09da0977ed1
David Bailey <david@davidabailey.com>
2020-03-19 15:57:15 -0700

    formatting

--- tests/uscensus/test_download_lodes.py
+++ tests/uscensus/test_download_lodes.py
@@ -7,6 +7,7 @@ ST = "ri"
 TYPE_ = "JT00"
 PART = "main"
 
+
 class TestDownloadLODES(unittest.TestCase):
     def test_download_lodes(self):
         download_lodes_od(ST, PART, TYPE_, YEAR)
--- tests/utils/test_download_file.py
+++ tests/utils/test_download_file.py
@@ -5,6 +5,7 @@ from dpd.utils import download_file
 URL = "https://www.google.com/humans.txt"
 LOCAL_FILENAME = "humans.txt"
 
+
 class TestDownloadFile(unittest.TestCase):
     def test_download_file(self):
         self.assertEqual(download_file(URL), LOCAL_FILENAME)
