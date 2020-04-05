50caa8aa61b74a6d7ba4e613a3c0499b4734ea3e
David Bailey <david@davidabailey.com>
2020-04-05 10:05:29 -0700

    working on vehicle classes

--- dpd/modeling/__init__.py
+++ dpd/modeling/__init__.py
@@ -1,4 +1,5 @@
 from .cost_dataframe import CostDataFrame
 from .origin_destination_dataframe import OriginDestinationDataFrame
 from .person_agent import PersonAgent
+from .vehicle import Vehicle
 from .zones import Zones
