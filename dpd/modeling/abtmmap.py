299dfbc56bbb7bd4ca206ce22b6030eea3364132
David Bailey <dbawssec@amazon.com>
2021-03-14 22:28:07 -0700

    add abtmmap

--- dpd/modeling/abtmmap.py
+++ dpd/modeling/abtmmap.py
@@ -5,12 +5,12 @@ class ABTMMap(Map):
         self.roads = map_.roads
         self.people = gpd.GeoDataFrame(columns=["geometry", "Person"])
         self.intersections["Intersection"] = self.intersections.apply(
-            lambda intersection: self.transform_intersection_to_agent_based(intersection),
-            axis=1
+            lambda intersection: self.transform_intersection_to_agent_based(
+                intersection
+            ),
+            axis=1,
         )
-        self.roads["Road"].map(
-            lambda road: self.transform_road_to_agent_based(road)
-        )
+        self.roads["Road"].map(lambda road: self.transform_road_to_agent_based(road))
         self.clear_lanes()
 
     def add_person(self, person):
@@ -24,11 +24,17 @@ class ABTMMap(Map):
         if hasattr(intersection, "Type"):
             intersection_type = intersection["Type"]
             if intersection_type == "Signal":
-                intersection = SignalIntersection(intersection["Intersection"], self.model)
+                intersection = SignalIntersection(
+                    intersection["Intersection"], self.model
+                )
             elif intersection_type == "Yield":
-                intersection = StopIntersection(intersection["Intersection"], self.model)
+                intersection = StopIntersection(
+                    intersection["Intersection"], self.model
+                )
             else:
-                intersection = YieldIntersection(intersection["Intersection"], self.model)
+                intersection = YieldIntersection(
+                    intersection["Intersection"], self.model
+                )
         else:
             intersection = YieldIntersection(intersection["Intersection"], self.model)
         self.model.schedule.add(intersection)
@@ -39,9 +45,13 @@ class ABTMMap(Map):
         this must be run after transform_intersection_to_agent_based creates new Intersection objects.
         """
         if road.input_intersection:
-            road.input_intersection = self.intersections.loc[road.input_intersection.name]["Intersection"]
+            road.input_intersection = self.intersections.loc[
+                road.input_intersection.name
+            ]["Intersection"]
         if road.output_intersection:
-            road.output_intersection = self.intersections.loc[road.output_intersection.name]["Intersection"]
+            road.output_intersection = self.intersections.loc[
+                road.output_intersection.name
+            ]["Intersection"]
 
     def clear_lanes(self):
         """
@@ -53,7 +63,9 @@ class ABTMMap(Map):
                     lane.occupants = []
 
     def refresh_people_geometries(self):
-        self.people["geometry"] = self.people["Person"].map(lambda person: person.geometry)
+        self.people["geometry"] = self.people["Person"].map(
+            lambda person: person.geometry
+        )
 
     def abtmplot(self, include_intersections=False, include_roads=False):
         self.refresh_people_geometries()
@@ -66,9 +78,16 @@ class ABTMMap(Map):
                 xy=row.geometry.centroid.coords[0],
                 horizontalalignment="center",
             )
-        self.plot(fig=fig, ax=ax, include_intersections=include_intersections, include_roads=include_roads)
+        self.plot(
+            fig=fig,
+            ax=ax,
+            include_intersections=include_intersections,
+            include_roads=include_roads,
+        )
 
-    def abtmplot_folium(self, include_intersections=False, include_roads=False, fields_people=None):
+    def abtmplot_folium(
+        self, include_intersections=False, include_roads=False, fields_people=None
+    ):
         self.refresh_people_geometries()
         folium_map = folium.Map(location=(38.9, -77), zoom_start=12)
         self.people.crs = "EPSG:4326"
@@ -80,4 +99,8 @@ class ABTMMap(Map):
         else:
             geojson = folium.GeoJson(self.people[["geometry"]])
         geojson.add_to(folium_map)
-        return self.plot_folium(folium_map=folium_map, include_intersections=include_intersections, include_roads=include_roads)
+        return self.plot_folium(
+            folium_map=folium_map,
+            include_intersections=include_intersections,
+            include_roads=include_roads,
+        )
