cb8902815883d42b01ca940afb23a5f4d8c5ad22
David Bailey <dbawssec@amazon.com>
2021-03-24 12:01:02 -0700

    add hsr model

--- dpd/modeling/gravity_model.py
+++ dpd/modeling/gravity_model.py
@@ -1,6 +1,9 @@
 class GravityModel():
-    def __init__(self, G):
+    def __init__(self, G=1, a=1, b=1, d=1):
         self.G = G
+        self.a = a
+        self.b = b
+        self.d = d
         
     def compute(mass_a, mass_b, distance):
-        return G * (mass_a, mass_b) / distance
+        return self.G * (mass_a**self.a, mass_b**self.b) / distance**self.d
---
+++ dpd/modeling/high_speed_rail_model.py
@@ -0,0 +1,1 @@
+HighSpeedRailModel = GravityModel(G=1, a=.8, b=.8, d=2)
