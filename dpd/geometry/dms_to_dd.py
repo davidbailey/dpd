aaaf42e6edcbc32699e4405c6a189e33d8492656
David Bailey <dbawssec@amazon.com>
2021-03-25 10:02:45 -0700

    bug fix in geo

--- dpd/geometry/dms_to_dd.py
+++ dpd/geometry/dms_to_dd.py
@@ -13,6 +13,6 @@ def dms_to_dd(coordinate):
         minutes = 0
         seconds = 0
     decimal_degrees = float(degrees) + float(minutes) / 60 + float(seconds) / (60 * 60)
-    if remainder == "E" or remainder == "N":
+    if remainder == "W" or remainder == "S":
         decimal_degrees *= -1
     return decimal_degrees
