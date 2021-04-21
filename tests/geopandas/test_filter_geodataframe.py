42896a360ba815711c1abd82d7c5e3dcf24739a2
David Bailey <dbawssec@amazon.com>
2021-04-20 23:22:15 -0700

    formatting

--- tests/geopandas/test_filter_geodataframe.py
+++ tests/geopandas/test_filter_geodataframe.py
@@ -8,9 +8,9 @@ from dpd.geopandas import filter_geodataframe
 
 class Test_filter_geodataframe(unittest.TestCase):
     def test_filter_geodataframe(self):
-        polygon = Polygon(box(0,0,10,10))
+        polygon = Polygon(box(0, 0, 10, 10))
         geodataframe = gpd.GeoDataFrame([polygon], columns=["geometry"])
-        filter_box = [-10,-10,5,5]
+        filter_box = [-10, -10, 5, 5]
         filtered_geodataframe = filter_geodataframe(geodataframe, filter_box)
         self.assertEqual((0.0, 0.0, 5.0, 5.0), filtered_geodataframe.geometry[0].bounds)
 
