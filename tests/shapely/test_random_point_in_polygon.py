551e51ec7f6ef21b81bebea827835f71a6074ef1
David Bailey <dbawssec@amazon.com>
2021-04-20 23:36:55 -0700

    add tests :)

---
+++ tests/shapely/test_random_point_in_polygon.py
@@ -0,0 +1,15 @@
+import unittest
+
+from shapely.geometry import Polygon, box
+
+from dpd.shapely import random_point_in_polygon
+
+
+class Test_random_point_in_polygon(unittest.TestCase):
+    def test_random_point_in_polygon(self):
+        polygon = Polygon(box(0, 0, 10, 10))
+        point = random_point_in_polygon(polygon)
+
+
+if __name__ == "__main__":
+    unittest.main()
