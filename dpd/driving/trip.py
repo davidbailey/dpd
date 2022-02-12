070c040265362ab6879bf0b75ef02cbe7a106907
David Bailey <davidbailey@users.noreply.github.com>
2022-02-12 13:30:17 -0800

    Update trip.py

--- dpd/driving/trip.py
+++ dpd/driving/trip.py
@@ -10,17 +10,17 @@ class Trip(MovingDict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def add_stop(self, geometry, distance, name="", arrival_time=None, departure_time=None, **kwargs):
+    def add_stop(self, geometry, distance_from_previous_stop=0, name="", arrival_time=None, departure_time=None, **kwargs):
         if  arrival_time:
-            self.data[arrival_time] = Stop(name=name, geometry=geometry, distance = distance, **kwargs)
+            self.data[arrival_time] = Stop(name=name, geometry=geometry, distance_from_previous_stop = distance_from_previous_stop, **kwargs)
         if departure_time:
-            self.data[departure_time] = Stop(name=name, geometry=geometry, distance = distance, **kwargs)
+            self.data[departure_time] = Stop(name=name, geometry=geometry, distance_from_previous_stop = distance_from_previous_stop, **kwargs)
 
     def plot_schedule(self, **kwargs):
-        geodataframe = self.to_geodataframe(columns=["name", "geometry", "distance"])
-        geodataframe["distance"].plot(**kwargs)
+        geodataframe = self.to_geodataframe(columns=["name", "geometry", "distance_from_previous_stop"])
+        geodataframe["distance_from_previous_stop"].plot(**kwargs)
         for idx, row in geodataframe.iterrows():
             plt.annotate(
                 text=row["name"],
-                xy=(idx, row["distance"])
+                xy=(idx, row["distance_from_previous_stop"])
             )
