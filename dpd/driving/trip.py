54dd2065d9dbdc0b06d55ce1e3dddbdcd9d845f0
David Bailey <davidbailey@users.noreply.github.com>
2022-02-11 21:30:04 -0800

    Update trip.py

--- dpd/driving/trip.py
+++ dpd/driving/trip.py
@@ -1,4 +1,5 @@
 from dpd.geometry import MovingDict
+import matplotlib.pyplot as plt
 
 class Trip(MovingDict):
     """
@@ -8,8 +9,17 @@ class Trip(MovingDict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def add_stop(geometry, arrival_time=None, departure_time=None, **kwargs):
+    def add_stop(self, geometry, distance, name="", arrival_time=None, departure_time=None, **kwargs):
         if  arrival_time:
-            self.data[arrival_time] = {"geometry": geometry, **kwargs}
+            self.data[arrival_time] = {"geometry": geometry, "distance": distance, "name": name, **kwargs}
         if departure_time:
-            self.data[departure_time] = {"geometry": geometry, **kwargs}
+            self.data[departure_time] = {"geometry": geometry, "distance": distance, "name": name, **kwargs}
+
+    def plot_schedule(self, **kwargs):
+        geodataframe = self.to_dataframe(columns=["geometry", "distance"])
+        geodataframe["distance"].plot(**kwargs)
+        for idx, row in geodataframe.iterrows():
+            plt.annotate(
+                text=row["name"],
+                xy=(idx, row["distance"])
+            )
