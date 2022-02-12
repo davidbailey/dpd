8c512a399615499d044752b7d30cfead04912551
David Bailey <davidbailey@users.noreply.github.com>
2022-02-11 20:48:23 -0800

    Update moving_dict.py

--- dpd/geometry/moving_dict.py
+++ dpd/geometry/moving_dict.py
@@ -3,18 +3,12 @@ from movingpandas import Trajectory
 
 class MovingDict(GeometricDict):
     """
-    A dictionsary of objects with a .geometry and a .t (datetime)
+    A dictionsary of objects with a .geometry and a datetime index.
     """
 
     def __init__(self, dict=None, crs=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def to_trajectory(self, traj_id, *args, **kwargs):
-        geodataframe = self.to_geodataframe()
+    def to_trajectory(self, traj_id, columns=["geometry"], *args, **kwargs):
+        geodataframe = self.to_geodataframe(columns=columns)
         return Trajectory(geodataframe, traj_id, *args, **kwargs)
-    
-    def to_geodataframe(self, columns=["geometry", "t"]):
-        return super().to_geodataframe(columns=columns, *args, **kwargs)
-
-    def to_json(self, columns=["geometry", "t"]):
-        return super().to_json(columns=columns, *args, **kwargs)
