6a4a51472bd1d2275d1446d672edb7a111b58c7e
David Bailey <davidbailey@users.noreply.github.com>
2022-02-15 13:35:29 -0800

    Create test_draw_arc.py

---
+++ tests/geometry/test_draw_arc.py
@@ -0,0 +1,21 @@
+from math import pi
+import unittest
+
+from shapely.geometry import LineString
+
+from dpd.geometry import draw_arc
+
+radius=10
+
+start_point = Point(0,radius)
+end_point = Point(radius,0)
+
+class TestDrawArc(unittest.TestCase):
+    def test_draw_arc(self):
+    arc = draw_arc(start_point, end_point, radius=radius, right_handed=True, number_of_points=16)
+    linestring = LineString(arc)
+    self.assertAlmostEqual(linestring.length, pi * 2 * radius / 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
