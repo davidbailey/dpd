f2c73b649fa5cd28678fcb5772eaa70af8479dcb
David Bailey <davidbailey@users.noreply.github.com>
2022-02-15 13:30:17 -0800

    Update __init__.py

--- dpd/geometry/__init__.py
+++ dpd/geometry/__init__.py
@@ -1,3 +1,4 @@
+from .find_center_from_points_and_radius import find_center_from_points_and_radius
 from .radius_of_curvature import radius_of_curvature
 from .dms_to_dd import dms_to_dd
 from .geometric_dict import GeometricDict
