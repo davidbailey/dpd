06615d93ce8f4ab4cdf43d0b893828c659bcbba1
David Bailey <davidbailey@users.noreply.github.com>
2022-02-15 13:31:32 -0800

    Create draw_arc.py

---
+++ dpd/geometry/draw_arc.py
@@ -0,0 +1,12 @@
+import numpy
+
+def draw_arc(start_point, end_point, radius, right_handed, number_of_points):
+    """
+    Returns an array of points from start_point to end_point along the circumference of a circle with a given radius
+    """
+    center = find_center_from_points_and_radius(start_point, end_point, radius, right_handed=right_handed)
+    chord = numpy.linspace([start_point.x, start_point.y], [end_point.x, end_point.y], number_of_points)
+    vectors = chord - center
+    magnitudes = numpy.linalg.norm(vectors, axis=1)
+    factors = radius / magnitudes
+    return (vectors.T * factors).T + center
