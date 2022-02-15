2dd6c75ca290248c7b393a01a890a488701a08ce
David Bailey <davidbailey@users.noreply.github.com>
2022-02-15 09:27:27 -0800

    Create straight_section.py

---
+++ dpd/driving/straight_section.py
@@ -0,0 +1,13 @@
+class StraightSection:
+    """
+    a straight section
+    start_point (Point): the start point of the section
+    end_point (Point): the end point of ths section
+    """
+    def __init__(
+        self,
+        start_point,
+        end_point,
+    ):
+        self.geometry = LineString([start_point, end_point])
+        self.speed_limit = None
