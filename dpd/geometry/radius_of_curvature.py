336d6b837878dd4b21dc474ad5a32e13e06e34a0
David Bailey <davidbailey@users.noreply.github.com>
2022-02-15 16:47:57 -0800

    Update radius_of_curvature.py

--- dpd/geometry/radius_of_curvature.py
+++ dpd/geometry/radius_of_curvature.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def radius_of_curvature(p0, p1, p2):
     """
-    Calculates the radius of curvurature between three points. Based on https://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points and https://en.wikipedia.org/wiki/Degree_of_curvature.
+    Calculates the radius of curvurature of a circle where p0 and p2 are on the circumference and p1 is the center. 
+    Based on https://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points and https://en.wikipedia.org/wiki/Degree_of_curvature.
     """
     a = np.array([p1[0] - p2[0], p1[1] - p2[1]])
     b = np.array([p1[0] - p0[0], p1[1] - p0[1]])
