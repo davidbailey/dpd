a92c01278ee648fce575e46a6d6ab4bf7cd2dc6b
David Bailey <davidbailey@users.noreply.github.com>
2022-02-16 20:32:06 -0800

    Update and rename test_radius_of_curvature.py to test_circle_from_three_circumference_points.py

---
+++ tests/geometry/test_circle_from_three_circumference_points.py
@@ -0,0 +1,13 @@
+import unittest
+
+from dpd.geometry import circle_from_three_circumference_points
+
+
+class TestCircleFromThreeCircumferencePoints(unittest.TestCase):
+    def test_circle_from_three_circumference_points(self):
+        self.assertEqual(circle_from_three_circumference_points((1,0), (-1,0), (0,1)), ((0.0, -0.0), 1.0))
+        self.assertEqual(circle_from_three_circumference_points((1,-6), (2,1), (5,2)), ((5.0, -3.0), 5.0))
+
+
+if __name__ == "__main__":
+    unittest.main()
