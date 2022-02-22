360677946e125cea71738110801260f26eafb006
David Bailey <david@davidabailey.com>
2022-02-21 16:23:39 -0800

    fixing things

--- dpd/driving/__init__.py
+++ dpd/driving/__init__.py
@@ -2,5 +2,4 @@ from .curved_section import CurvedSection
 from .route import Route
 from .stop import Stop
 from .straight_section import StraightSection
-from .trip import Trip
 from .vehicle import Vehicle
