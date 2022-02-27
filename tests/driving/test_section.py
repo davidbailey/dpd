0266ad4d82c3621580bdfbea6a9553c61182a4df
David Bailey <david@davidabailey.com>
2022-02-26 17:28:11 -0800

    fix test

---
+++ tests/driving/test_section.py
@@ -0,0 +1,16 @@
+import unittest
+
+from shapely.geometry import Point
+
+from dpd.driving import Section
+
+
+class TestSection(unittest.TestCase):
+    def test_section(self):
+        straight = Section(Point(0, 0), Point(1, 0))
+        self.assertEqual(section.speed_limit, None)
+        self.assertEqual(section.geometry.length, 1.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
