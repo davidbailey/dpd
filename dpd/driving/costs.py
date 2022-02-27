1d9fa32cada9e1ae80ef7ebb57770204b25aaae9
David Bailey <david@davidabailey.com>
2022-02-26 18:40:11 -0800

    fix test

---
+++ dpd/driving/costs.py
@@ -0,0 +1,11 @@
+elevations = {
+    "surface": {"dollars_per_meter": 1000},
+    "underground": {"dollars_per_meter": 2000},
+    "elevated": {"dollars_per_meter": 1500},
+}
+
+modes = ["rail", "road"]
+
+
+def segment_cost(mode, elevation, length):
+    pass
