803459b20a58e837b8f7b0af937113c0b8a74a47
David Bailey <david@davidabailey.com>
2022-02-28 22:44:23 -0800

    add uniform_points_in_polygon

--- dpd/shapely/__init__.py
+++ dpd/shapely/__init__.py
@@ -1,3 +1,4 @@
 from .cut import cut
 from .find_way_in_ways import find_way_in_ways
 from .random_point_in_polygon import random_point_in_polygon
+from .uniform_points_in_polygon import uniform_points_in_polygon
