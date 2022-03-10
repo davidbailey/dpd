f16e3618b0ad87f7c9d037073157c6a5cafe226d
David Bailey <david@davidabailey.com>
2022-03-09 22:58:50 -0800

    fix things

--- dpd/geometry/__init__.py
+++ dpd/geometry/__init__.py
@@ -1,8 +1,6 @@
 from .dms_to_dd import dms_to_dd
 from .draw_arc import draw_arc
-from .circle_from_three_circumference_points import (
-    circle_from_three_circumference_points,
-)
+from .circle_from_three_points import circle_from_three_points
 from .circle_center_from_points_and_radius import circle_center_from_points_and_radius
 from .geometric_dict import GeometricDict
 from .moving_dict import MovingDict
