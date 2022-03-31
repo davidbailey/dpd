5d41b11b46a2ff32ecde0a8df8d2f796ccdf8a34
David Bailey <david@davidabailey.com>
2022-03-31 00:46:08 -0700

    formatting

--- dpd/analysis/values/__init__.py
+++ dpd/analysis/values/__init__.py
@@ -1,2 +1,1 @@
 from .transportation_gov import Value_of_Reduced_Fatalties_and_Injuries
-
--- dpd/analysis/values/transportation_gov.py
+++ dpd/analysis/values/transportation_gov.py
@@ -6,7 +6,16 @@ usd = Currency("USD", 2020, 0.07).discount()
 # https://www.transportation.gov/sites/dot.gov/files/2022-03/Benefit%20Cost%20Analysis%20Guidance%202022%20%28Revised%29.pdf
 
 Value_of_Reduced_Fatalties_and_Injuries = Series(
-    [3900 * usd, 77200 * usd, 151100 * usd, 554800 * usd, 11600000 * usd, 210300 * usd, 302600 * usd, 12837400 * usd],
+    [
+        3900 * usd,
+        77200 * usd,
+        151100 * usd,
+        554800 * usd,
+        11600000 * usd,
+        210300 * usd,
+        302600 * usd,
+        12837400 * usd,
+    ],
     index=[
         "O - No Injury",
         "C - Possible Injury",
@@ -17,5 +26,5 @@ Value_of_Reduced_Fatalties_and_Injuries = Series(
         "Injury Crash",
         "Fatal Crash",
     ],
-    name="Value of Reduced Fatalties and Injuries"
+    name="Value of Reduced Fatalties and Injuries",
 )
