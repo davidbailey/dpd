b983866450ca8b5b1b7e66073004c0a216d6df1b
David Bailey <davidbailey@users.noreply.github.com>
2022-04-13 21:17:03 -0700

    Create simulated_dynamic_body.py

---
+++ dpd/mechanics/simulated_dynamic_body.py
@@ -0,0 +1,33 @@
+from astropy import units
+
+from .simulated_kinematic_body_with_acceleration import SimulatedKinematicBodyWithAcceleration
+
+class SimulatedDynamicBody(SimulatedKinematicBodyWithAcceleration):
+    """
+    A class to simulate a dynamic body. Provides methods to move the body with constant power.
+    """
+
+    def __init__(
+        self,
+        power,
+        mass,
+        acceleration_limit = None,
+        *args,
+        **kwargs
+    ):
+        self.power = power
+        self.mass = mass
+        super().__init__(*args, **kwargs)
+
+    def step_acceleration(self, write):
+        next_acceleration = self.power / (self.mass * self.current_speed)
+        if self.acceleration_limit:
+            next_acceleration = min(next_acceleration, self.acceleration_limit)
+        if write:
+            self.current_acceleration = next_acceleration
+        return next_acceleration
+
+    def step(self, write=True):
+        next_time = self.step_time(write=write)
+        next_distance = self.step_distance(write=write)
+        return next_time, next_distance
