10092fb3a92a89cb446d4e5c6d9133305715041a
David Bailey <davidbailey@users.noreply.github.com>
2022-04-14 09:50:25 -0700

    Update test_simulated_kinematic_body_with_acceleration.py

--- tests/mechanics/test_simulated_kinematic_body_with_acceleration.py
+++ tests/mechanics/test_simulated_kinematic_body_with_acceleration.py
@@ -7,7 +7,7 @@ from dpd.mechanics import SimulatedKinematicBodyWithAcceleration
 
 class TestSimulatedKinematicBodyWithAcceleration(unittest.TestCase):
     def test_simulated_kinematic_body_with_acceleration(self):
-        body = TestSimulatedKinematicBodyWithAcceleration(
+        body = SimulatedKinematicBodyWithAcceleration(
             initial_acceleration=1 * units.meter / units.second**2,
             velocity_limit=1 * units.meter / units.second,
         )
