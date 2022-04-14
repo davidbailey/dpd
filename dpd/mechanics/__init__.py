447df7b8520d31f4df52999f6a1bf8ce184c3de9
David Bailey <davidbailey@users.noreply.github.com>
2022-04-13 21:31:46 -0700

    Update __init__.py

--- dpd/mechanics/__init__.py
+++ dpd/mechanics/__init__.py
@@ -1,1 +1,5 @@
 from .move import move
+from .simulated_dynamic_body import SimulatedDynamicBody
+from .simulated_kinematic_body import SimulatedKinematicBody
+from .simulated_kinematic_body_with_acceleration import SimulatedKinematicBodyWithAcceleration
+from .simulated_kinematic_deceleration import SimulatedKinematicDeceleration
