60a0cb9bae969629f3b64e4a4387789217e57f82
David Bailey <davidbailey@users.noreply.github.com>
2022-04-13 21:19:05 -0700

    Create simulated_kinematic_deceleration.py

---
+++ dpd/mechanics/simulated_kinematic_deceleration.py
@@ -0,0 +1,21 @@
+class SimulatedKinematicDeceleration:
+    """
+    Provides methods to decelerate the body with constant deceleration.
+    """
+
+    def __init__(
+        self,
+        max_deceleration,
+    ):
+        self.max_deceleration = max_deceleration
+
+    def stopping_distance(self, final_velocity):
+        return (final_velocity ** 2 - self.current_velocity ** 2) / (
+            2 * self.max_deceleration
+        )
+    
+    def decelerate(self, distance, final_velocity):
+        self.current_time += 2 * distance / (final_velocity + self.current_velocity)
+        self.current_distance += distance
+        self.current_velocity = final_velocity
+        return self.current_time, self.current_distance
