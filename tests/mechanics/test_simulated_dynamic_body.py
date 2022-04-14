38cfa7174f0ec30ccffbd80164b358cb5d1a1d6e
David Bailey <davidbailey@users.noreply.github.com>
2022-04-14 12:49:33 -0700

    Update test_simulated_dynamic_body.py

--- tests/mechanics/test_simulated_dynamic_body.py
+++ tests/mechanics/test_simulated_dynamic_body.py
@@ -7,7 +7,11 @@ from dpd.mechanics import SimulatedDynamicBody
 
 class TestSimulatedDynamicBody(unittest.TestCase):
     def test_simulated_dynamic_body(self):
-        body = SimulatedDynamicBody(power=1 * units.watt, mass=1 * units.kilogram)
+        body = SimulatedDynamicBody(
+            power=1 * units.watt,
+            mass=1 * units.kilogram, 
+            acceleration_limit=1 * units.meter / units.second**2
+        )
         next_time, next_distance = body.step()
         self.assertEqual(next_time, 1 * units.second)
         self.assertEqual(next_distance, 1 * units.meter)
