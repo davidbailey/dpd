b3f4d405c092c88372486e78d2f8df16f944c237
David Bailey <david@davidabailey.com>
2022-04-14 09:32:41 -0700

    formatting

--- tests/mechanics/test_bicycle.py
+++ tests/mechanics/test_bicycle.py
@@ -8,7 +8,12 @@ from dpd.mechanics import Bicycle
 class TestBicycle(unittest.TestCase):
     def test_bicycle(self):
         truck = Bicycle(
-            x=0, y=0, theta=0, velocity=1, acceleration=0, vehicle_length=29.5 * units.imperial.foot
+            x=0,
+            y=0,
+            theta=0,
+            velocity=1,
+            acceleration=0,
+            vehicle_length=29.5 * units.imperial.foot,
         )
         radius = truck.get_radius_from_vehicle_length_and_steering_angle(
             steering_angle=35 * units.degree
--- tests/mechanics/test_simulated_kinematic_body_with_acceleration.py
+++ tests/mechanics/test_simulated_kinematic_body_with_acceleration.py
@@ -8,8 +8,8 @@ from dpd.mechanics import SimulatedKinematicBodyWithAcceleration
 class TestSimulatedKinematicBodyWithAcceleration(unittest.TestCase):
     def test_simulated_kinematic_body_with_acceleration(self):
         body = TestSimulatedKinematicBodyWithAcceleration(
-          initial_acceleration=1 * units.meter / units.second**2,
-          velocity_limit = 1 * units.meter / units.second
+            initial_acceleration=1 * units.meter / units.second**2,
+            velocity_limit=1 * units.meter / units.second,
         )
         next_time, next_distance = body.step()
         self.assertEqual(next_time, 1 * units.second)
