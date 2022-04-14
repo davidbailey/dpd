1dad7c7cb25867d77bf6ef3daccf7953e0a6356b
David Bailey <davidbailey@users.noreply.github.com>
2022-04-14 12:26:51 -0700

    Update test_bicycle.py

--- tests/mechanics/test_bicycle.py
+++ tests/mechanics/test_bicycle.py
@@ -8,11 +8,11 @@ from dpd.mechanics import Bicycle
 class TestBicycle(unittest.TestCase):
     def test_bicycle(self):
         truck = Bicycle(
-            x=0,
-            y=0,
-            theta=0,
-            velocity=1,
-            acceleration=0,
+            x=0 * units.meter,
+            y=0 * units.meter,
+            theta=0 * units.degree,
+            velocity=1 * units.meter / units.second,
+            acceleration=0 * units.meter / units.second**2,
             vehicle_length=29.5 * units.imperial.foot,
         )
         radius = truck.get_radius_from_vehicle_length_and_steering_angle(
