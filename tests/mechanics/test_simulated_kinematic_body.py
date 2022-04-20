542578958a1ea4c4294a6fbe73c24d42ee92fca1
David Bailey <davidbailey@users.noreply.github.com>
2022-04-20 16:28:50 -0700

    Update test_simulated_kinematic_body.py

--- tests/mechanics/test_simulated_kinematic_body.py
+++ tests/mechanics/test_simulated_kinematic_body.py
@@ -7,7 +7,8 @@ from dpd.mechanics import SimulatedKinematicBody
 
 class TestSimulatedKinematicBody(unittest.TestCase):
     def test_simulated_kinematic_body(self):
-        body = SimulatedKinematicBody(initial_velocity=1 * units.meter / units.second)
+        body = SimulatedKinematicBody()
+        body.current_velocity = 1 * units.meter / units.second
         next_time, next_distance = body.step()
         self.assertEqual(next_time, 1 * units.second)
         self.assertEqual(next_distance, 1 * units.meter)
