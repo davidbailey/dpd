53d4fe7f6e6f508cd268199525e672d4fb69385c
David Bailey <davidbailey@users.noreply.github.com>
2022-04-25 14:07:57 -0700

    Update and rename test_route.py.bak to test_route.py

--- tests/driving/test_route.py
+++ tests/driving/test_route.py
@@ -1,6 +1,6 @@
 import unittest
 
-from dpd.driving import Route, Vehicle
+from dpd.driving import Route
 from dpd.osm import OSM
 
 
@@ -11,9 +11,7 @@ osm.download_relation(relation)
 
 class TestRoute(unittest.TestCase):
     def test_route(self):
-        self.vehicle = Vehicle(24.5872, 0.5, -0.5)
         self.route = Route.from_osm_relation(osm, relation)
-        self.trip = self.route.drive(self.vehicle, 45)
 
 
 if __name__ == "__main__":
