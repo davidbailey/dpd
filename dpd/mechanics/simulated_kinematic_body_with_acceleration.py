6a446900eab3c8ea31e40781c9b26093ad42d1ed
David Bailey <davidbailey@users.noreply.github.com>
2022-07-05 21:42:57 -0700

    Update simulated_kinematic_body_with_acceleration.py

--- dpd/mechanics/simulated_kinematic_body_with_acceleration.py
+++ dpd/mechanics/simulated_kinematic_body_with_acceleration.py
@@ -5,12 +5,13 @@ from .simulated_kinematic_body import SimulatedKinematicBody
 
 class SimulatedKinematicBodyWithAcceleration(SimulatedKinematicBody):
     """
-    A class to simulate a kinematic body. Provides methods to move the body with constant acceleration.
+    A class to simulate a kinematic body. Provides methods to move the body with constant acceleration and decelerate the body with constant deceleration.
     """
 
-    def __init__(self, velocity_limit=None, *args, **kwargs):
+    def __init__(self, velocity_limit=None, max_deceleration, *args, **kwargs):
         self.current_acceleration = 0 * units.meter / units.second**2
         self.velocity_limit = velocity_limit
+        self.max_deceleration = max_deceleration
         super().__init__(*args, **kwargs)
 
     def step_acceleration(self, write):
@@ -30,3 +31,14 @@ class SimulatedKinematicBodyWithAcceleration(SimulatedKinematicBody):
         next_time = self.step_time(write=write)
         next_distance = self.step_distance(write=write)
         return next_time, next_distance
+
+    def stopping_distance(self, final_velocity):
+        return (final_velocity**2 - self.current_velocity**2) / (
+            2 * self.max_deceleration
+        )
+
+    def decelerate(self, distance, final_velocity):
+        self.current_time += 2 * distance / (final_velocity + self.current_velocity)
+        self.current_distance += distance
+        self.current_velocity = final_velocity
+        return self.current_time, self.current_distance
