87261483b5477c7a36a072dae87fe9d80a4d75d2
David Bailey <davidbailey@users.noreply.github.com>
2022-07-08 20:51:01 -0700

    Update body.py

--- dpd/mechanics/body.py
+++ dpd/mechanics/body.py
@@ -1,12 +1,11 @@
-from astropy import units
 from mesa import Agent
-from uuid import uuid4
 
 class Body(Agent):
     """
+    A n-dimensional body.
     """
 
-    def __init__(self, initial_position, unique_id=None, *args, **kwargs):
-        unique_id = unique_id if unique_id else uuid4()
-        super().__init__(unique_id, *args, **kwargs)
+    def __init__(self, initial_position, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.position = initial_position
+        
