3f0212d1db7a4551bc8bb92ebd46e1e5cb571f23
David Bailey <davidbailey@users.noreply.github.com>
2022-07-08 18:35:36 -0700

    Update body.py

--- dpd/mechanics/body.py
+++ dpd/mechanics/body.py
@@ -6,7 +6,7 @@ class Body(Agent):
     """
     """
 
-    def __init__(self, initial_position, model, unique_id=None):
-        self.unique_id = unique_id if unique_id else uuid4()
-        super().__init__(self.unique_id, model)
+    def __init__(self, initial_position, unique_id=None, *args, **kwargs):
+        unique_id = unique_id if unique_id else uuid4()
+        super().__init__(unique_id, *args, **kwargs)
         self.position = initial_position
