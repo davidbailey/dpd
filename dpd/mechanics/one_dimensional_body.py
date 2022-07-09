621679ee49042786bd758fcacb6a4dc4b2b86a26
David Bailey <davidbailey@users.noreply.github.com>
2022-07-08 18:29:42 -0700

    Update one_dimensional_body.py

--- dpd/mechanics/one_dimensional_body.py
+++ dpd/mechanics/one_dimensional_body.py
@@ -6,7 +6,7 @@ class OneDimensionalBody(Agent):
     """
     """
 
-    def __init__(self, model):
-        self.unique_id = uuid4()
+    def __init__(self, model, unique_id=None, initial_position=None):
+        self.unique_id = unique_id if unique_id else uuid4()
         super().__init__(self.unique_id, model)
-        self.position = 0 * units.meter
+        self.position = initial_position if initial_position else 0 * units.meter
