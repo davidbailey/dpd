6549a1118ea0805cf5d62377604cec2e10f6f4d9
David Bailey <davidbailey@users.noreply.github.com>
2022-07-08 22:23:19 -0700

    Update kinematic_body.py

--- dpd/mechanics/kinematic_body.py
+++ dpd/mechanics/kinematic_body.py
@@ -5,16 +5,18 @@ class KinematicBody(Body):
     A class to simulate a kinematic body. Provides methods to move the body with constant velocity.
     """
 
-    def __init__(self, velocity, max_position=None, *args, **kwargs):
+    def __init__(self, velocity, max_position=None, min_position=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.velocity = velocity
         self.max_position = max_position
+        self.min_position = min_position
 
     def step_position(self):
         self.position = self.position + self.velocity
         if self.max_position and self.position > self.max_position
             self.position = self.max_position
-            
+        if self.min_position and self.position < self.min_position
+            self.position = self.min_position
 
     def step(self):
         self.step_position()
