722bd1e42c189297426a3008389070e854f2b6a5
David Bailey <davidbailey@users.noreply.github.com>
2022-07-13 13:37:38 -0700

    Update mode_choice_model.py

--- dpd/modeling/mode_choice_model.py
+++ dpd/modeling/mode_choice_model.py
@@ -2,9 +2,9 @@ from numpy.random import choice
 
 
 class ModeChoiceModel:
-    def __init__(self, modes=[], probabilities=[]):
-        self.modes = modes
-        self.probabilities = probabilities
+    def __init__(self):
+        self.modes = []
+        self.probabilities = []
 
     def add_mode(self, mode, probability):
         self.modes.append(mode)
