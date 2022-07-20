ffe9fa7de728c920a45e190f338c1224dd5c702c
David Bailey <davidbailey@users.noreply.github.com>
2022-07-19 23:35:09 -0700

    Update vehicles.py

--- dpd/driving/vehicles.py
+++ dpd/driving/vehicles.py
@@ -1,61 +1,38 @@
-import logging
+from functools import partial
 
 from astropy import units
-from pandas import Series, isna, read_csv
+import ipywidgets as widgets
+from IPython.display import display
+from pandas import read_csv
 
-from dpd.driving import Vehicle
 
+from .dynamic_body_stopper import DynamicBodyStopper
 
-class Vehicles(Series):
-    def read_csv(
-        url,
-        default_power,
-        default_mass,
-        default_max_speed,
-        default_acceleration_limit,
-        default_max_deceleration,
-    ):
-        dataframe = read_csv(url)
-        dataframe.index = dataframe.Manufacturer + " " + dataframe.Model
-        dataframe.sort_index(inplace=True)
-        vehicles = []
-        for index, row in dataframe.iterrows():
-            logging.debug("Adding %s" % (index,))
-            power = (
-                row["Power"].split(" ")[0] * units.Unit(row["Power"].split(" ")[1])
-                if not isna(row["Power"])
-                else default_power
-            )
-            mass = (
-                row["Mass"].split(" ")[0] * units.Unit(row["Mass"].split(" ")[1])
-                if not isna(row["Mass"])
-                else default_mass
-            )
-            max_speed = (
-                row["Maximum speed"].split(" ")[0]
-                * units.Unit(row["Maximum speed"].split(" ")[1])
-                if not isna(row["Maximum speed"])
-                else default_max_speed
-            )
-            acceleration_limit = (
-                row["Acceleration"].split(" ")[0]
-                * units.Unit(row["Acceleration"].split(" ")[1])
-                if not isna(row["Acceleration"])
-                else default_acceleration_limit
-            )
-            max_deceleration = (
-                row["Deceleration"].split(" ")[0]
-                * units.Unit(row["Deceleration"].split(" ")[1])
-                if not isna(row["Deceleration"])
-                else default_max_deceleration
-            )
-            vehicles.append(
-                Vehicle(
-                    power=power,
-                    mass=mass,
-                    max_speed=max_speed,
-                    acceleration_limit=acceleration_limit,
-                    max_deceleration=max_deceleration,
-                )
-            )
-        return Series(data=vehicles, index=dataframe.index)
+class Vehicles:
+    def __init__(self, url):
+        self.vehicles = read_csv(url)
+        self.vehicles.index = self.vehicles.Manufacturer + " " + self.vehicles.Model
+        self.vehicles.sort_index(inplace=True)
+        self.vehicles.dropna(axis=0, subset=["Maximum speed", "Power", "Mass"], inplace=True)
+        vehicle_widget = widgets.Dropdown(
+            options=self.vehicles.index,
+            description="Vehicle",
+        )
+        vehicle_widget.observe(self.vehicle_dropdown_observer, names="value")
+        display(vehicle_widget)
+        self.vehicle = self.create_vehicle(self.vehicles.index[0])
+
+    def vehicle_dropdown_observer(self, value):
+        self.vehicle = self.create_vehicle(value.new)
+
+    def create_vehicle(self, index):
+        maximum_speed = self.vehicles.loc[index]["Maximum speed"].split(" ")[0] * units.Unit(
+            self.vehicles.loc[index]["Maximum speed"].split(" ")[1]
+        )
+        power = self.vehicles.loc[index]["Power"].split(" ")[0] * units.Unit(
+            self.vehicles.loc[index]["Power"].split(" ")[1]
+        )
+        mass = self.vehicles.loc[index]["Mass"].split(" ")[0] * units.Unit(
+            self.vehicles.loc[index]["Mass"].split(" ")[1]
+        )
+        return partial(DynamicBodyStopper, maximum_speed=maximum_speed, power=power, mass=mass)
