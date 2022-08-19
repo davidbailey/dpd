cce9e9f2382645d8339045d0b7ecc58d1a70be94
David Bailey <davidbailey@users.noreply.github.com>
2022-08-18 18:11:11 -0700

    Update body.py

--- dpd/mechanics/body.py
+++ dpd/mechanics/body.py
@@ -5,7 +5,7 @@ class Body(Agent):
     An n-dimensional body.
     """
 
-    def __init__(self, initial_position=None, *args, **kwargs):
+    def __init__(self, initial_position, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.initial_position = initial_position
         self.position = initial_position
