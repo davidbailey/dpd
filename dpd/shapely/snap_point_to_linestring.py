e278a7a8f24c1e5306d8284981165c639bed2b57
David Bailey <davidbailey@users.noreply.github.com>
2022-10-16 15:59:48 -0700

    Create snap_point_to_linestring.py

---
+++ dpd/shapely/snap_point_to_linestring.py
@@ -0,0 +1,11 @@
+from shapely.geometry import LineString
+from dpd.shapely import cut, linemerge
+
+def snap_point_to_linestring(linestring, point):
+    """
+    "Snaps the point to the closest point along the linestring. Adds that point to the linestring. Returns the new linestring and the closest point on the linestring.
+    """
+    distance = linestring.project(point)
+    closest_point_on_linestring = linestring.interpolate(distance)
+    new_linestring = linemerge(cut(linestring, distance))
+    return new_linestring, closest_point_on_linestring
