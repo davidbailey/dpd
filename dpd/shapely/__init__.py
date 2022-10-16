d95fb955f321c94f6017be2e3219eb9d46ab1099
David Bailey <davidbailey@users.noreply.github.com>
2022-10-16 16:00:08 -0700

    Update __init__.py

--- dpd/shapely/__init__.py
+++ dpd/shapely/__init__.py
@@ -1,4 +1,5 @@
 from .cut import cut
 from .find_way_in_ways import find_way_in_ways
 from .random_point_in_polygon import random_point_in_polygon
+from .snap_point_to_linestring import snap_point_to_linestring
 from .uniform_points_in_polygon import uniform_points_in_polygon
