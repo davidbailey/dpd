f95a33ca5dc4abcee1d282ac02801e8ffaea62cd
David Bailey <david@davidabailey.com>
2022-11-03 19:28:12 -0700

    test work?

---
+++ tests/mechanics/test_dynamic_body.py
@@ -0,0 +1,24 @@
+import unittest
+
+from astropy import units
+
+from dpd.mechanics import DynamicBody
+
+
+class TestSimulatedDynamicBody(unittest.TestCase):
+    def test_simulated_dynamic_body(self):
+        """
+        body = SimulatedDynamicBody(
+            power=1 * units.watt,
+            mass=1 * units.kilogram,
+            acceleration_limit=1 * units.meter / units.second**2,
+        )
+        next_time, next_distance = body.step()
+        self.assertEqual(next_time, 1 * units.second)
+        self.assertEqual(next_distance, 1 * units.meter)
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/mechanics/test_kinematic_body_with_acceleration.py
@@ -0,0 +1,23 @@
+import unittest
+
+from astropy import units
+
+from dpd.mechanics import KinematicBodyWithAcceleration
+
+
+class TestSimulatedKinematicBodyWithAcceleration(unittest.TestCase):
+    def test_simulated_kinematic_body_with_acceleration(self):
+        """
+        body = SimulatedKinematicBodyWithAcceleration(
+            velocity_limit=1 * units.meter / units.second,
+        )
+        body.current_acceleration = 1 * units.meter / units.second**2
+        next_time, next_distance = body.step()
+        self.assertEqual(next_time, 1 * units.second)
+        self.assertEqual(next_distance, 1 * units.meter)
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
