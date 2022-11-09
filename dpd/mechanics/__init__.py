4725b6f29d9202f62de7421b4888203b79d738fe
David Bailey <david@davidabailey.com>
2022-11-08 17:09:42 -0800

    add data collectors

--- dpd/mechanics/__init__.py
+++ dpd/mechanics/__init__.py
@@ -4,3 +4,4 @@ from .move import move
 from .dynamic_body import DynamicBody
 from .kinematic_body import KinematicBody
 from .kinematic_body_with_acceleration import KinematicBodyWithAcceleration
+from .datacollection import BodyDataCollector, KinematicBodyDataCollector, KinematicBodyWithAccelerationDataCollector, DynamicBodyDataCollector
