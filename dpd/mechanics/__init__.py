8a48de569d6e2cec951f3f0382d0fb54cb7c8a03
David Bailey <david@davidabailey.com>
2022-11-08 17:10:06 -0800

    add data collectors

--- dpd/mechanics/__init__.py
+++ dpd/mechanics/__init__.py
@@ -4,4 +4,9 @@ from .move import move
 from .dynamic_body import DynamicBody
 from .kinematic_body import KinematicBody
 from .kinematic_body_with_acceleration import KinematicBodyWithAcceleration
-from .datacollection import BodyDataCollector, KinematicBodyDataCollector, KinematicBodyWithAccelerationDataCollector, DynamicBodyDataCollector
+from .datacollection import (
+    BodyDataCollector,
+    KinematicBodyDataCollector,
+    KinematicBodyWithAccelerationDataCollector,
+    DynamicBodyDataCollector,
+)
--- dpd/mechanics/datacollection.py
+++ dpd/mechanics/datacollection.py
@@ -4,10 +4,16 @@ body_agent_reporters = {"position": "position"}
 BodyDataCollector = DataCollector(agent_reporters=body_agent_reporters)
 
 kinematic_body_agent_reporters = body_agent_reporters | {"velocity": "velocity"}
-KinematicBodyDataCollector = DataCollector(agent_reporters=kinematic_body_agent_reporters)
+KinematicBodyDataCollector = DataCollector(
+    agent_reporters=kinematic_body_agent_reporters
+)
 
-kinematic_body_with_acceleration_agent_reporters = kinematic_body_agent_reporters | {"acceleration": "acceleration"}
-KinematicBodyWithAccelerationDataCollector = DataCollector(agent_reporters=kinematic_body_with_acceleration_agent_reporters)
+kinematic_body_with_acceleration_agent_reporters = kinematic_body_agent_reporters | {
+    "acceleration": "acceleration"
+}
+KinematicBodyWithAccelerationDataCollector = DataCollector(
+    agent_reporters=kinematic_body_with_acceleration_agent_reporters
+)
 
 dynamic_body_agent_reporters = kinematic_body_with_acceleration_agent_reporters | {}
 DynamicBodyDataCollector = DataCollector(agent_reporters=dynamic_body_agent_reporters)
