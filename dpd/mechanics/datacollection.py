03def1ec8e63d692d54ed53ea13ee4713fe1d4e4
David Bailey <david@davidabailey.com>
2022-11-08 17:09:54 -0800

    add data collectors

---
+++ dpd/mechanics/datacollection.py
@@ -0,0 +1,13 @@
+from mesa.datacollection import DataCollector
+
+body_agent_reporters = {"position": "position"}
+BodyDataCollector = DataCollector(agent_reporters=body_agent_reporters)
+
+kinematic_body_agent_reporters = body_agent_reporters | {"velocity": "velocity"}
+KinematicBodyDataCollector = DataCollector(agent_reporters=kinematic_body_agent_reporters)
+
+kinematic_body_with_acceleration_agent_reporters = kinematic_body_agent_reporters | {"acceleration": "acceleration"}
+KinematicBodyWithAccelerationDataCollector = DataCollector(agent_reporters=kinematic_body_with_acceleration_agent_reporters)
+
+dynamic_body_agent_reporters = kinematic_body_with_acceleration_agent_reporters | {}
+DynamicBodyDataCollector = DataCollector(agent_reporters=dynamic_body_agent_reporters)
