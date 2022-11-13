f7b4680129bbeba7b378578fd72fd9161a00f1b4
David Bailey <david@davidabailey.com>
2022-11-13 12:33:18 -0800

    add kinematic body tests

--- tests/mechanics/test_kinematic_body.py
+++ tests/mechanics/test_kinematic_body.py
@@ -1,20 +1,62 @@
 import unittest
+from uuid import uuid4
 
-# from astropy import units
+from astropy import units
+import numpy
+from numpy.testing import assert_array_equal
 
-# from dpd.mechanics import KinematicBody
+from dpd.mechanics import KinematicBody
+from dpd.mechanics.datacollection import KinematicBodyDataCollector
+from dpd.modeling import TransportationModel
 
 
-class TestSimulatedKinematicBody(unittest.TestCase):
-    def test_simulated_kinematic_body(self):
-        """
-        body = SimulatedKinematicBody()
-        body.current_velocity = 1 * units.meter / units.second
-        next_time, next_distance = body.step()
-        self.assertEqual(next_time, 1 * units.second)
-        self.assertEqual(next_distance, 1 * units.meter)
-        """
-        pass
+class TestKinematic1DInt(unittest.TestCase):
+    def test_body_1d_int(self):
+        model = TransportationModel(datacollector=KinematicBodyDataCollector)
+        body = KinematicBody(
+            model=model, unique_id=uuid4(), initial_position=1, initial_velocity=1
+        )
+        model.step()
+        self.assertEqual(body.position, 1)
+
+
+class TestKinematicBody1DQuantity(unittest.TestCase):
+    def test_body_1d_quantity(self):
+        model = TransportationModel(datacollector=KinematicBodyDataCollector)
+        body = KinematicBody(
+            model=model,
+            unique_id=uuid4(),
+            initial_position=1 * units.meter,
+            initial_velocity=1 * units.meter / units.second,
+        )
+        model.step()
+        self.assertEqual(body.position, 1 * units.meter)
+
+
+class TestKinematicBody2DInt(unittest.TestCase):
+    def test_body_2d_int(self):
+        model = TransportationModel(datacollector=KinematicBodyDataCollector)
+        body = KinematicBody(
+            model=model,
+            unique_id=uuid4(),
+            initial_position=numpy.array([1, 1]),
+            initial_velocity=numpy.array([1, 1]),
+        )
+        model.step()
+        assert_array_equal(body.position, numpy.array([1, 1]))
+
+
+class TestKinematicBody2DQuantity(unittest.TestCase):
+    def test_body_2d_quantity(self):
+        model = TransportationModel(datacollector=KinematicBodyDataCollector)
+        body = KinematicBody(
+            model=model,
+            unique_id=uuid4(),
+            initial_position=numpy.array([1, 1]) * units.meter,
+            initial_velocity=numpy.array([1, 1]) * units.meter / units.second,
+        )
+        model.step()
+        assert_array_equal(body.position, numpy.array([1, 1]) * units.meter)
 
 
 if __name__ == "__main__":
