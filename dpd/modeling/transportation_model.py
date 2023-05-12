806910cc1ed35c117f56f88c87df9296888b1170
David Bailey <davidbailey@users.noreply.github.com>
2023-05-12 11:57:15 -0700

    Update transportation_model.py

--- dpd/modeling/transportation_model.py
+++ dpd/modeling/transportation_model.py
@@ -4,11 +4,11 @@ from mesa.time import RandomActivation
 
 
 class TransportationModel(Model):
-    def __init__(self, datacollector=None, time_unit=1):
-        if datacollector is not None:
-            self.datacollector = datacollector
-        else:
-            pass
+    def __init__(self, agent_reporters=None, time_unit=1):
+        self.datacollector = DataCollector(
+            agent_reporters=agent_reporters,
+            model_reporters={"time": lambda m: m.schedule.time * m.time_unit}
+        )
         self.time_unit = time_unit
         self.schedule = RandomActivation(self)
 
@@ -20,3 +20,9 @@ class TransportationModel(Model):
         while self.schedule.agents:
             self.step()
             self.datacollector.collect(self)
+
+     def get_dataframe(self):
+        mvdf = self.datacollector.get_model_vars_dataframe()
+        mvdf.index.set_names("Step", inplace=True)
+        avdf = model.datacollector.get_agent_vars_dataframe()
+        return avdf.merge(mvdf, left_index=True, right_index=True)
