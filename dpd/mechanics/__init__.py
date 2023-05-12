23d0f3456586d2cf8b88caad982315f19af94dac
David Bailey <davidbailey@users.noreply.github.com>
2023-05-12 11:43:07 -0700

    Update __init__.py

--- dpd/mechanics/__init__.py
+++ dpd/mechanics/__init__.py
@@ -1,6 +1,5 @@
 from .bicycle import Bicycle
 from .body import Body
-from .datacollection import BODY_AGENT_REPORTERS, KINEMATIC_BODY_AGENT_REPORTERS, KINEMATIC_BODY_WITH_ACCELERATION_AGENT_REPORTERS, DYNAMIC_BODY_AGENT_REPORTERS
 from .move import move
 from .dynamic_body import DynamicBody
 from .kinematic_body import KinematicBody
