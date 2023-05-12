c36b71125fcccbae95559a112578486893975f34
David Bailey <davidbailey@users.noreply.github.com>
2023-05-12 11:40:54 -0700

    Update transportation_model.py

--- dpd/modeling/transportation_model.py
+++ dpd/modeling/transportation_model.py
@@ -1,12 +1,6 @@
 from mesa import Model
 from mesa.time import RandomActivation
 
-BODY_AGENT_REPORTERS = {"position": "position"}
-KINEMATIC_BODY_AGENT_REPORTERS = BODY_AGENT_REPORTERS | {"velocity": "velocity"}
-KINEMATIC_BODY_WITH_ACCELERATION_AGENT_REPORTERS = KINEMATIC_BODY_AGENT_REPORTERS | {
-    "acceleration": "acceleration"
-}
-DYNAMIC_BODY_AGENT_REPORTERS = KINEMATIC_BODY_WITH_ACCELERATION_AGENT_REPORTERS | {}
 
 class TransportationModel(Model):
     def __init__(self, datacollector=None, time_unit=1):
