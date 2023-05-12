03ccda2166d7ba67613a8661a8ab40fc9401935f
David Bailey <davidbailey@users.noreply.github.com>
2023-05-12 11:49:01 -0700

    Update transportation_model.py

--- dpd/modeling/transportation_model.py
+++ dpd/modeling/transportation_model.py
@@ -1,10 +1,14 @@
 from mesa import Model
+from mesa.datacollection import DataCollector
 from mesa.time import RandomActivation
 
 
 class TransportationModel(Model):
     def __init__(self, datacollector=None, time_unit=1):
-        self.datacollector = datacollector
+        if datacollector is not None:
+            self.datacollector = datacollector
+        else:
+            pass
         self.time_unit = time_unit
         self.schedule = RandomActivation(self)
 
