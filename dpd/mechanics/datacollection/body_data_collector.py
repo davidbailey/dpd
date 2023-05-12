40ccb5dca64f7458419de719e1d44a095960e49f
David Bailey <davidbailey@users.noreply.github.com>
2023-05-12 11:43:18 -0700

    Delete dpd/mechanics/datacollection directory

--- dpd/mechanics/datacollection/body_data_collector.py
+++
@@ -1,4 +0,0 @@
-from mesa.datacollection import DataCollector
-
-body_agent_reporters = {"position": "position"}
-BodyDataCollector = DataCollector(agent_reporters=body_agent_reporters)
--- dpd/mechanics/datacollection/kinematic_body_data_collector.py
+++
@@ -1,7 +0,0 @@
-from mesa.datacollection import DataCollector
-
-body_agent_reporters = {"position": "position"}
-kinematic_body_agent_reporters = body_agent_reporters | {"velocity": "velocity"}
-KinematicBodyDataCollector = DataCollector(
-    agent_reporters=kinematic_body_agent_reporters
-)
