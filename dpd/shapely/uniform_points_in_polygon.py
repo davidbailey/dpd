116816e7160f4f08e1d76fe6aa20e6d981f3e85d
David Bailey <davidbailey@users.noreply.github.com>
2023-05-15 15:52:23 -0700

    Update uniform_points_in_polygon.py

--- dpd/shapely/uniform_points_in_polygon.py
+++ dpd/shapely/uniform_points_in_polygon.py
@@ -2,11 +2,11 @@ import numpy as np
 from shapely.geometry import Point
 
 
-def uniform_points_in_polygon(polygon):
+def uniform_points_in_polygon(polygon, num=50):
     bbox = polygon.bounds
     points = []
-    for i in np.linspace(bbox[0], bbox[2]):
-        for j in np.linspace(bbox[1], bbox[3]):
+    for i in np.linspace(bbox[0], bbox[2], num=num):
+        for j in np.linspace(bbox[1], bbox[3], num=num):
             if polygon.intersects(Point(i, j)):
                 points.append(Point(i, j))
     return points
