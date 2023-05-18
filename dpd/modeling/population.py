b938d815394fb72cf7bd12eded476381098997cf
David Bailey <david@davidabailey.com>
2023-05-18 14:15:43 -0700

    update population

--- dpd/modeling/population.py
+++ dpd/modeling/population.py
@@ -1,10 +1,12 @@
-class Population:
-    """
-    A class to hold People.
-    """
+from pandas import DataFrame
 
-    def __init__(self):
-        super().__init__()
 
-    def from_zones():
-        pass
+class Population(DataFrame):
+    @staticmethod
+    def from_trip_dataframe(trip_dataframe):
+        population = []
+        for origin in trip_dataframe.index:
+            for destination in trip_dataframe.columns:
+                for number_of_trips in range(trip_dataframe[destination][origin]):
+                    population.append({"origin": origin, "destination": destination})
+        return Population(people)
