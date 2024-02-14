728e4df9d8b0439f231b4d049e29c13b99dd4cb3
David Bailey <davidbailey@users.noreply.github.com>
2024-02-14 07:13:05 -0800

    Update dynamic_body.py

--- dpd/mechanics/dynamic_body.py
+++ dpd/mechanics/dynamic_body.py
@@ -1,6 +1,5 @@
-import astropy
-import numpy
 from numpy import minimum, maximum
+from numpy.linalg import norm
 
 from .kinematic_body_with_acceleration import KinematicBodyWithAcceleration
 
@@ -20,14 +19,10 @@ class DynamicBody(KinematicBodyWithAcceleration):
         self.min_acceleration = min_acceleration
 
     def step_acceleration(self):
-        if isinstance(self.velocity, astropy.units.quantity.Quantity):
-            if not self.velocity.value.any():
-                self.acceleration = self.initial_acceleration
-                return
-        elif not numpy.any(self.velocity):
+        if norm(self.velocity) == 0.0:
             self.acceleration = self.initial_acceleration
-            return
-        self.acceleration = self.power / (self.mass * self.velocity)
+        else:
+            self.acceleration = self.power / (self.mass * self.velocity)
         if self.max_acceleration is not None:
             self.acceleration = minimum(self.acceleration, self.max_acceleration)
         if self.min_acceleration is not None:
