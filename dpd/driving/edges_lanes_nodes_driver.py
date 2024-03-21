663195eea65f8d9217fcb4ecf8659b708a618656
David Bailey <davidbailey@users.noreply.github.com>
2024-03-20 20:07:48 -0700

    Update edges_lanes_nodes_driver.py

--- dpd/driving/edges_lanes_nodes_driver.py
+++ dpd/driving/edges_lanes_nodes_driver.py
@@ -10,6 +10,21 @@ class EdgesLanesNodesDriver(EdgesLanesDriver):
         self.begin_next_node()
         super().__init__(*args, **kwargs)
 
+    @staticmethod
+    def from_node_ids(edges_dict, nodes_dict, node_ids, *args, **kwargs):
+        """
+        Preplans the nodes and edges for a Driver based on Node IDs and a nodes_dict and edges_dict
+        
+        edges_dict (dict): a dictionary with a tuple of (node_id[i], node_id[i+1] as the index for each edge value, this is often a networkx.DiGraph or a pandas.DataFrame
+        nodes_dict (dict): a dictionary with node_id as the index for each node value, this is often a networkx.DiGraph or a pandas.DataFrame
+        node_ids ([node_id]): a list of Node IDs that describes the route a Driver takes
+        """
+        edges = [
+            edges_dict[(node_ids[i], node_ids[i + 1])]["object"] for i in range(len(node_ids) - 1)
+        ]
+        nodes = [nodes_dict[node_id]["object"] for node_id in node_ids]
+        return EdgesLanesNodesDriver(edges=edges, nodes=nodes, *args, **kwargs)
+
     def begin_next_edge(self, *args, **kwargs):
         super().begin_next_edge(*args, **kwargs)
         if self.next_node.entry_velocity is not None:
