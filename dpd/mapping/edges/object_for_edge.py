78f92f1b0c7a1330d76d6782193445356b97106b
David Bailey <davidbailey@users.noreply.github.com>
2024-03-23 09:51:37 -0700

    Create object_for_edge.py

---
+++ dpd/mapping/edges/object_for_edge.py
@@ -0,0 +1,13 @@
+from .edge import Edge
+
+def object_for_edge(edge):
+    """
+    edge
+
+    """
+    return Edge(
+        geometry=edge["geometry"],
+        length=edge["length"],
+        lanes=edge["lanes"],
+        maxspeed=edge["maxspeed"],
+    )
