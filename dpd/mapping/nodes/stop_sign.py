4f879d282807c92df87387fa3b0dac4c2085ed56
David Bailey <davidbailey@users.noreply.github.com>
2024-03-29 17:23:53 -0700

    Update stop_sign.py

--- dpd/mapping/nodes/stop_sign.py
+++ dpd/mapping/nodes/stop_sign.py
@@ -1,9 +1,7 @@
-from mesa import Agent
-
 from .node import Node
 
 
-class StopSign(Node, Agent):
-    def __init__(self, *args, **kwargs):
+class StopSign(Node):
+    def __init__(self, entry_velocity = 0, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.entry_velocity = 0
+        self.entry_velocity = entry_velocity
