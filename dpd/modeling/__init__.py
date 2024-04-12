3a5e194281d789af2b9d01552f21a48c2c0a955c
David Bailey <davidbailey@users.noreply.github.com>
2024-04-11 20:57:30 -0700

    Update __init__.py

--- dpd/modeling/__init__.py
+++ dpd/modeling/__init__.py
@@ -5,7 +5,6 @@ from .contour_dataframe import contour_dataframe
 from .distance_dataframe import DistanceDataFrame
 from .mode_choice_model import ModeChoiceModel
 from .people import People
-from .people_flask_app import people_flask_app
 from .population import Population
 from .transportation_model import TransportationModel
 from .trip_dataframe import TripDataFrame
