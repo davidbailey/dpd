955d0db7961b536c925a41f3f4a2ef6c08f66ed3
David Bailey <davidbailey@users.noreply.github.com>
2024-04-25 11:01:08 -0700

    Update __init__.py

--- dpd/driving/__init__.py
+++ dpd/driving/__init__.py
@@ -1,14 +1,6 @@
-from .dynamic_body_driver import DynamicBodyDriver
-from .dynamic_body_stopper import DynamicBodyStopper
 from .edges_driver import EdgesDriver
 from .edges_lanes_driver import EdgesLanesDriver
 from .edges_lanes_nodes_driver import EdgesLanesNodesDriver
-from .kinematic_body_driver import KinematicBodyDriver
-from .kinematic_body_stopper import KinematicBodyStopper
-from .kinematic_body_with_acceleration_driver import KinematicBodyWithAccelerationDriver
-from .kinematic_body_with_acceleration_stopper import (
-    KinematicBodyWithAccelerationStopper,
-)
 from .network import Network
 from .route import Route
 from .schedule import Schedule
