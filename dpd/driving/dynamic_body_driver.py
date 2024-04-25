c3a2bdfc36efe17bef8c284bf2b3b606af5f465d
David Bailey <davidbailey@users.noreply.github.com>
2024-04-25 11:01:28 -0700

    Delete dpd/driving/dynamic_body_driver.py

--- dpd/driving/dynamic_body_driver.py
+++
@@ -1,7 +0,0 @@
-from dpd.mechanics import DynamicBody
-
-from .kinematic_body_with_acceleration_driver import KinematicBodyWithAccelerationDriver
-
-
-class DynamicBodyDriver(DynamicBody, KinematicBodyWithAccelerationDriver):
-    pass
