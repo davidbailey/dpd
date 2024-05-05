b6d2599ea51d0913454568ec8ddc1cbd7d3ed1cc
David Bailey <davidbailey@users.noreply.github.com>
2024-05-04 20:49:15 -0700

    Update __init__.py

--- dpd/analysis/values/__init__.py
+++ dpd/analysis/values/__init__.py
@@ -1,4 +1,4 @@
-from .rail import LightRail, HeavyRail
+from .rail import HeavyRail, LightRail
 from .transportation_gov import (
     Value_of_Reduced_Fatalities_and_Injuries,
     Value_of_Travel_Time_Savings,
